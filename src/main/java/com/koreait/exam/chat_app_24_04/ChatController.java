e9dda3780fcf68bf42d0c3a14bfa1c0e8c9bab9a
jihyeon2434 <ubis970420@gmail.com>
2024-04-11 15:53:44 +0900

    ChatMessage 클래스 생성

---
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
@@ -0,0 +1,16 @@
+package com.koreait.exam.chat_app_24_04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+    ChatMessage message = new ChatMessage("홍길동","안녕하세요");
+
+    @PostMapping ("/writeMessage")
+    @ResponseBody
+    public RsData<ChatMessage> writeMessage() {
+        return new RsData("S-1","메세지가 작성됨",message);
+    }
+}
